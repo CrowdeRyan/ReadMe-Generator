9fa0dfce990dc8d05c3987083d4f0cf757e583a0
Ryan Crowder <crowderyan@gmail.com>
2021-06-26 17:16:34 -0400

    added prompt and questions

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 // README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+const promptUser = () => {
+  return inquirer.prompt([
+    // WHEN I enter my project title
+    // THEN this is displayed as the title of the README
+    {
+      type: "input",
+      name: "Title",
+      message: "What is the title of your project?",
+    },
+
+    // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+    // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+    {
+      type: "input",
+      name: "description",
+      message: "Describe your project:",
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "How do you install your application?",
+    },
+    {
+      type: "input",
+      name: "uses",
+      message: "How do you use your application?",
+    },
+    {
+      type: "input",
+      name: "guidelines",
+      message: "Enter your guide lines for contribution:",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "How does one test your application?",
+    },
+
+    // WHEN I choose a license for my application from a list of options
+    // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under.
+    {
+      type: "checkbox",
+      message: "Which license did you use?",
+      name: "license",
+      choices: ["MIT", "", "", ""],
+    },
+
+    // WHEN I enter my GitHub username
+    // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username:",
+    },
+    // WHEN I enter my email address
+    // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address:",
+    },
+  ]);
+};
+
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 const generateReadme = (answers) => `#${answers.title}
 Description: ${answers.description}
   License: ${answers.license}
-  
+
   Table of Contents
   <a href="#install">Installation</a>
   <a href="#use">Usage</a>
